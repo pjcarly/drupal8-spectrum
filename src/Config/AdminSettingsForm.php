a83f3b48af3a2091af9f0cc28d1da607df02d602
Pieterjan C <pieterjan.carly@gmail.com>
2016-03-19 00:30:17 +0100

    Refactor

--- src/Config/AdminSettingsForm.php
+++ src/Config/AdminSettingsForm.php
@@ -1,8 +1,4 @@
 <?php
-/**
- * @file
- * Contains \Drupal\spectrum\Form\SpectrumAdminSettingsForm.
- */
 
 namespace Drupal\spectrum\Config;
 
@@ -10,32 +6,27 @@ use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
 
-/**
- * Configure Spectrum settings for this site.
- */
-class AdminSettingsForm extends ConfigFormBase {
 
-  /**
-   * {@inheritdoc}
-   */
-  public function getFormID() {
+class AdminSettingsForm extends ConfigFormBase
+{
+
+  public function getFormID()
+  {
     return 'spectrum_admin_settings';
   }
 
-  protected function getEditableConfigNames() {
+  protected function getEditableConfigNames()
+  {
     return ['spectrum.settings'];
   }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function buildForm(array $form, FormStateInterface $form_state) {
+  public function buildForm(array $form, FormStateInterface $form_state)
+  {
     $config = $this->config('spectrum.settings');
 
     $form['sendgrid_api_key'] = array(
       '#type' => 'textfield',
       '#title' => t('Sendgrid API Key'),
-      '#required' => TRUE,
       '#default_value' => $config->get('sendgrid_api_key'),
       '#description' => t('The API key for your Sendgrid account.')
     );
@@ -43,21 +34,16 @@ class AdminSettingsForm extends ConfigFormBase {
     return parent::buildForm($form, $form_state);
   }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
+  public function validateForm(array &$form, FormStateInterface $form_state)
+  {
     parent::validateForm($form, $form_state);
   }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
+  public function submitForm(array &$form, FormStateInterface $form_state)
+  {
     $config = $this->config('spectrum.settings');
-    $config
-      ->set('sendgrid_api_key', $form_state->getValue('sendgrid_api_key'))
-      ->save();
+    $config->set('sendgrid_api_key', $form_state->getValue('sendgrid_api_key'));
+    $config->save();
 
     parent::submitForm($form, $form_state);
   }
