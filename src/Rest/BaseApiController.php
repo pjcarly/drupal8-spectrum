0c856072e82cf438d2f374c49aceb0cc675ed81b
Pieterjan Carly <pieterjan.carly@gmail.com>
2017-11-11 01:57:47 +0100

    Refactor BaseApiController for Api permissions

--- src/Rest/BaseApiController.php
+++ src/Rest/BaseApiController.php
@@ -17,26 +17,120 @@ class BaseApiController implements ContainerAwareInterface
 {
   use ContainerAwareTrait;
 
-  protected $response;
-
-  public function __construct()
-  {
-    $this->response = new Response(null, 404, array());
-  }
-
-  public function handle(RouteMatchInterface $route_match, Request $request, $api = NULL, $slug = NULL, $action = NULL)
+  protected function addCorsHeaders(RouteMatchInterface $routeMatch, Request $request, Response $response)
   {
     // We let the request pass through Drupal's internals, so other modules can apply correct headers
     $config = \Drupal::service('config.factory');
     $alias_manager = \Drupal::service('path.alias_manager');
-    $path_matcher = new PathMatcher($config, $route_match);
+    $path_matcher = new PathMatcher($config, $routeMatch);
 
     $kernel = \Drupal::service('http_kernel.basic');
-    $event = new FilterResponseEvent($kernel, $request, null, $this->response);
+    $event = new FilterResponseEvent($kernel, $request, null, $response);
 
     $cors = new CorsResponseEventSubscriber($config, $alias_manager, $path_matcher);
     $cors->addCorsHeaders($event);
+  }
 
-    return $this->response;
+  // Returns string representing
+  // C = POST
+  // R = GET
+  // U = PATCH
+  // D = DELETE
+  private function getAccessForRequestMethod(Request $request) : string
+  {
+    $access = '';
+    switch($request->getMethod())
+    {
+      case 'GET':
+        $access = 'R';
+        break;
+      case 'POST':
+        $access = 'C';
+        break;
+      case 'PATCH':
+        $access = 'U';
+        break;
+      case 'DELETE':
+        $access = 'D';
+        break;
+    }
+
+    return $access;
+  }
+
+  protected function apiPermissionExists(string $route, string $api)
+  {
+    $permissionExists = false;
+
+    // Workaround because of custom permission bug in Drupal
+    if(function_exists('get_permission_checker'))
+    {
+      $permissionChecker = get_permission_checker();
+      $permissionExists = $permissionChecker::apiPermissionExists($route, $api);
+    }
+    else
+    {
+      $permissionExists = false;
+    }
+
+    return $permissionExists;
+  }
+
+  protected function userHasApiPermission(Request $request, string $route, string $api) : bool
+  {
+    $access = $this->getAccessForRequestMethod($request);
+
+    $currentUser = \Drupal::currentUser();
+    $permissionGranted = false;
+
+    // Workaround because of custom permission bug in Drupal
+    if(function_exists('get_permission_checker'))
+    {
+      $permissionChecker = get_permission_checker();
+
+      $userRoles = $currentUser->getRoles();
+      foreach($userRoles as $userRole)
+      {
+        if($permissionChecker::roleHasApiPermission($userRole, $route, $api, $access))
+        {
+          $permissionGranted = true;
+          break;
+        }
+      }
+    }
+    else
+    {
+      $permissionGranted = false;
+    }
+
+    return $permissionGranted;
+  }
+
+  public function handle(RouteMatchInterface $routeMatch, Request $request, $api = NULL, $slug = NULL, $action = NULL)
+  {
+    $response = new Response(null, 500, array());
+
+    if($this->apiPermissionExists($routeMatch->routeName))
+    {
+      if($this->userHasApiPermission($request, $routeMatch->routeName, $api))
+      {
+        $response->setStatusCode(204); // OK, no content
+        $response->setContent = null;
+      }
+      else
+      {
+        $response->setStatusCode(423); // Failed Locked
+        $response->setContent(null);
+      }
+    }
+    else
+    {
+      $response->setStatusCode(404); // Failed not found
+      $response->setContent(null);
+    }
+
+    $this->addCorsHeaders($routeMatch, $request, $response);
+
+    return $response;
   }
 }
