59c91b61053f2627ca62e7cd8b7f4051762a2f73
Pieterjan Carly <pieterjan.carly@gmail.com>
2018-01-10 15:55:46 +0100

    Added returntypes and uuid generator

--- src/Utils/StringUtils.php
+++ src/Utils/StringUtils.php
@@ -6,34 +6,45 @@ use Stringy\StaticStringy as Stringy;
 
 class StringUtils
 {
-  static function camelize($input)
+  static function camelize(string $input) : string
   {
     return Stringy::camelize($input);
   }
 
-  static function dasherize($input)
+  static function dasherize(string $input) : string
   {
     return Stringy::dasherize($input);
   }
 
-  static function underscore($input)
+  static function underscore(string $input) : string
   {
     return Stringy::underscored($input);
   }
 
-  static function hasSpaces($string)
+  static function hasSpaces(string $string) : bool
   {
     return preg_match('/\s/', $string);
   }
 
-  static function contains($haystack, $needle)
+  static function contains(string $haystack, string $needle) : bool
   {
     return strpos($haystack, $needle) !== false;
   }
 
-  static function isAlphaNumericWithoutSpaces($string)
+  static function isAlphaNumericWithoutSpaces(string $string) : bool
   {
     return preg_match('/^[a-z0-9 .\-]+$/i', $string);
+  }
+
+  static function generateUUID() : string
+  {
+    return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+        mt_rand( 0, 0xffff ),
+        mt_rand( 0, 0x0fff ) | 0x4000,
+        mt_rand( 0, 0x3fff ) | 0x8000,
+        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+    );
   }
 
   /**
@@ -46,7 +57,7 @@ class StringUtils
    * @param string $needle
    * @return array or false
    */
-  static function positionsOfSubstring($haystack, $needle)
+  static function positionsOfSubstring(string $haystack, string $needle)
   {
     $s = 0;
     $i = 0;
