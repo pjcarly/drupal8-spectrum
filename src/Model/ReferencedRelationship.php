4bb0163208e344110fd8d2cd506aaec66f56bd7d
pjcarly <pieterjan.carly@gmail.com>
2018-05-30 15:36:47 +0200

    Load possible exteded modelclass for ReferencedRelationsihp

--- src/Model/ReferencedRelationship.php
+++ src/Model/ReferencedRelationship.php
@@ -2,10 +2,13 @@
 
 namespace Drupal\spectrum\Model;
 
+use Drupal\spectrum\Query\ModelQuery;
 use Drupal\spectrum\Query\Condition;
 
 class ReferencedRelationship extends Relationship
 {
+  protected static $cachedModelTypes = [];
+
   public $fieldRelationship;
   public $fieldRelationshipName;
   public $modelType;
@@ -13,19 +16,32 @@ class ReferencedRelationship extends Relationship
   public function __construct($relationshipName, $modelType, $fieldRelationshipName, $cascade = 0)
   {
     parent::__construct($relationshipName, $cascade);
-    $this->modelType = $modelType;
+    $this->modelType = static::getModelType($modelType);
     $this->fieldRelationshipName = $fieldRelationshipName;
-    $this->fieldRelationship = $modelType::getRelationship($fieldRelationshipName);
+    $this->fieldRelationship = $this->modelType::getRelationship($fieldRelationshipName);
   }
 
-  public function getCondition()
+  public function getCondition() : Condition
   {
     return new Condition($this->fieldRelationship->relationshipField, 'IN', null);
   }
 
-  public function getRelationshipQuery()
+  public function getRelationshipQuery() : ModelQuery
   {
     $modelType = $this->modelType;
     return $modelType::getModelQuery();
+  }
+
+  /**
+   * Because modeltypes can be exteded in each application, we load the modelType based on the one available in the get_registered_model_classes function
+   */
+  public static function getModelType(string $modelType) : string
+  {
+    if(!array_key_exists($modelType, static::$cachedModelTypes))
+    {
+      static::$cachedModelTypes[$modelType] = Model::getModelClassForEntityAndBundle($modelType::$entityType, $modelType::$bundle);
+    }
+
+    return static::$cachedModelTypes[$modelType];
   }
 }
