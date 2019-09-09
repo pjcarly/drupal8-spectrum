a6eb8d4cdf89c57625c60a4339875c7d4cac63f1
Pieterjan C <pieterjan.carly@gmail.com>
2019-09-09 17:20:50 +0200

    ParentAccessPolicy ->isNew fix

--- src/Permissions/AccessPolicy/ParentAccessPolicy.php
+++ src/Permissions/AccessPolicy/ParentAccessPolicy.php
@@ -121,7 +121,7 @@ class ParentAccessPolicy extends AccessPolicyBase
 
     $checkFields = array_unique($checkFields);
 
-    return ($this->isNew() || $model->someFieldsChanged($checkFields));
+    return ($model->isNew() || $model->someFieldsChanged($checkFields));
   }
 
   /**
